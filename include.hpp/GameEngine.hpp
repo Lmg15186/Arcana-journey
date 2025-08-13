4ea02ad8c7ec69d4d2818dadbd82d2c625b08dca
Lmg15186 <15186@ait.nsw.edu.au>
2025-08-13 19:46:13 +1000

    Update GameEngine.hpp

--- include.hpp/GameEngine.hpp
+++ include.hpp/GameEngine.hpp
@@ -1,6 +1,23 @@
-GameEngine.hpp
-Player.hpp
-TarotCard.hpp
-SignProfile.hpp
-ConsoleUI.hpp
+#ifndef GAME_ENGINE_HPP
+#define GAME_ENGINE_HPP
 
+#include "Player.hpp"
+#include "TarotCard.hpp"
+#include "ConsoleUI.hpp"
+#include <vector>
+
+class GameEngine {
+private:
+    Player player;
+    std::vector<TarotCard*> deck;
+    int currentCardIndex;
+
+public:
+    GameEngine();
+    void startGame();
+    void drawCard();
+    void processTurn(int choice);
+    void endGame();
+};
+
+#endif
