d76f0bc17a2005dd3733e509fb737de5a27cba03
Lmg15186 <15186@ait.nsw.edu.au>
2025-08-13 19:46:44 +1000

    Update Player.hpp

--- include.hpp/Player.hpp
+++ include.hpp/Player.hpp
@@ -1,8 +1,20 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include <string>
+
 class Player {
 private:
     std::string name;
+    std::string zodiacSign;
+    int wisdomPoints;
+
 public:
-    Player(std::string name);
-    void displayInfo();
+    Player();
+    Player(std::string name, std::string zodiacSign);
+    void addWisdom(int points);
+    int getWisdomPoints() const;
+    std::string getZodiacSign() const;
 };
 
+#endif
