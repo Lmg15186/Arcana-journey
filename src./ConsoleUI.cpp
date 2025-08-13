6d57e088b1c0ed633725071226f32354fb798f27
Lmg15186 <15186@ait.nsw.edu.au>
2025-08-13 19:58:49 +1000

    Update ConsoleUI.cpp

--- src./ConsoleUI.cpp
+++ src./ConsoleUI.cpp
@@ -1,1 +1,28 @@
+#include "ConsoleUI.hpp"
+#include <iostream>
+using namespace std;
+
+void ConsoleUI::showWelcome() {
+    cout << "Welcome to The Arcana Journey!" << endl;
+}
+
+void ConsoleUI::showCard(const TarotCard& card) {
+    cout << card.getName() << endl;
+    cout << card.getDescription() << endl;
+    int i = 1;
+    for (auto& opt : card.getOptions()) {
+        cout << i++ << ". " << opt << endl;
+    }
+}
+
+int ConsoleUI::getPlayerChoice() {
+    int choice;
+    cout << "Enter your choice: ";
+    cin >> choice;
+    return choice;
+}
+
+void ConsoleUI::showFinalResult(int wisdomPoints) {
+    cout << "Final Wisdom Score: " << wisdomPoints << endl;
+}
 
