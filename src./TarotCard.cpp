8ee634e78950d2b33517fcf61ed467dda3a50be6
Lmg15186 <15186@ait.nsw.edu.au>
2025-08-13 19:58:17 +1000

    Update TarotCard.cpp

--- src./TarotCard.cpp
+++ src./TarotCard.cpp
@@ -1,1 +1,9 @@
+#include "TarotCard.hpp"
+
+TarotCard::TarotCard(std::string name, std::string description, std::vector<std::string> options)
+    : name(name), description(description), options(options) {}
+
+std::string TarotCard::getName() const { return name; }
+std::string TarotCard::getDescription() const { return description; }
+std::vector<std::string> TarotCard::getOptions() const { return options; }
 
