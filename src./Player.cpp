d5c66980f2e76cfa76ae2a17bfbd4f36f6ea2bf0
Lmg15186 <15186@ait.nsw.edu.au>
2025-08-13 19:40:39 +1000

    Update Player.cpp

--- src./Player.cpp
+++ src./Player.cpp
@@ -1,1 +1,10 @@
+#include "Player.hpp"
+#include <iostream>
+using namespace std;
+
+Player::Player(string name) : name(name) {}
+
+void Player::displayInfo() {
+    cout << "Player: " << name << endl;
+}
 
