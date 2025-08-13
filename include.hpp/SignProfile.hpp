8c943a74529fcd6b41b6299ab6c74fa8acc4ed08
Lmg15186 <15186@ait.nsw.edu.au>
2025-08-13 19:47:28 +1000

    Update SignProfile.hpp

--- include.hpp/SignProfile.hpp
+++ include.hpp/SignProfile.hpp
@@ -1,1 +1,16 @@
+#ifndef SIGN_PROFILE_HPP
+#define SIGN_PROFILE_HPP
+
+#include <string>
+
+class SignProfile {
+protected:
+    std::string signName;
+public:
+    SignProfile(std::string signName);
+    virtual bool matchesChoice(int choice) = 0; // pure virtual
+    std::string getSignName() const;
+};
+
+#endif
 
