cb6f6b2bbc98687e902296ca209239d3072c5879
Lmg15186 <15186@ait.nsw.edu.au>
2025-08-13 19:47:02 +1000

    Update TarotCard.hpp

--- include.hpp/TarotCard.hpp
+++ include.hpp/TarotCard.hpp
@@ -1,1 +1,22 @@
+#ifndef TAROT_CARD_HPP
+#define TAROT_CARD_HPP
+
+#include <string>
+#include <vector>
+
+class TarotCard {
+protected:
+    std::string name;
+    std::string description;
+    std::vector<std::string> options;
+
+public:
+    TarotCard(std::string name, std::string description, std::vector<std::string> options);
+    virtual int evaluateChoice(int choice, std::string zodiacSign) = 0; // pure virtual
+    std::string getName() const;
+    std::string getDescription() const;
+    std::vector<std::string> getOptions() const;
+};
+
+#endif
 
