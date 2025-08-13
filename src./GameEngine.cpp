33bf64213401e3d049b67d031809d6b9f0ef8dae
Lmg15186 <15186@ait.nsw.edu.au>
2025-08-13 19:57:45 +1000

    Update GameEngine.cpp

--- src./GameEngine.cpp
+++ src./GameEngine.cpp
@@ -1,1 +1,23 @@
+#include "GameEngine.hpp"
+#include <iostream>
+using namespace std;
+
+GameEngine::GameEngine() : currentCardIndex(0) {}
+
+void GameEngine::startGame() {
+    ConsoleUI::showWelcome();
+    // TODO: Ask player name and sign, initialize deck
+}
+
+void GameEngine::drawCard() {
+    // TODO: Draw and display a tarot card
+}
+
+void GameEngine::processTurn(int choice) {
+    // TODO: Evaluate choice and update wisdom points
+}
+
+void GameEngine::endGame() {
+    // TODO: Show final score and reading
+}
 
