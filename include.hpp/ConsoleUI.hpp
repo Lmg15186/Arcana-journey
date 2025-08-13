633148da478ee049feb14417e6314293111aa9a1
Lmg15186 <15186@ait.nsw.edu.au>
2025-08-13 19:47:51 +1000

    Update ConsoleUI.hpp

--- include.hpp/ConsoleUI.hpp
+++ include.hpp/ConsoleUI.hpp
@@ -1,1 +1,16 @@
+#ifndef CONSOLE_UI_HPP
+#define CONSOLE_UI_HPP
+
+#include "TarotCard.hpp"
+#include <string>
+
+class ConsoleUI {
+public:
+    static void showWelcome();
+    static void showCard(const TarotCard& card);
+    static int getPlayerChoice();
+    static void showFinalResult(int wisdomPoints);
+};
+
+#endif
 
