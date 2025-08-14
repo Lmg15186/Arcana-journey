20ddbadf9d33b1ef63d3272ac8a0cc3559febe45
Lmg15186 <15186@ait.nsw.edu.au>
2025-08-14 11:48:44 +1000

    Update ConsoleUI.hpp

--- include/ConsoleUI.hpp
+++ include/ConsoleUI.hpp
@@ -6,11 +6,17 @@
 
 class ConsoleUI {
 public:
+    // Mostra a tela de boas-vindas
     static void showWelcome();
+
+    // Mostra uma carta de tarot com suas opções
     static void showCard(const TarotCard& card);
+
+    // Lê a escolha do jogador
     static int getPlayerChoice();
+
+    // Mostra o resultado final do jogo
     static void showFinalResult(int wisdomPoints);
 };
 
 #endif
-
