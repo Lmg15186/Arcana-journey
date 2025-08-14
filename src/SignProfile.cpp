1e3f1a962f3848a71d06fed838cf86937c81e89c
Lmg15186 <15186@ait.nsw.edu.au>
2025-08-14 11:54:22 +1000

    Update SignProfile.cpp

--- src/SignProfile.cpp
+++ src/SignProfile.cpp
@@ -1,6 +1,9 @@
-
 #include "SignProfile.hpp"
 
+// Construtor
 SignProfile::SignProfile(std::string signName) : signName(signName) {}
 
-std::string SignProfile::getSignName() const { return signName; }
+// Retorna o nome do signo
+std::string SignProfile::getSignName() const {
+    return signName;
+}
